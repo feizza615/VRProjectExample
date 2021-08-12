e5abc9086f7eff14f0f1f84b6669be5cc706a4ee
Feizza Fazilatun <feizza01@gmail.com>
2021-08-11 20:03:56 -0400

    done

---
+++ script.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+      var COLORS = ['red', 'green', 'blue'];
+      this.el.addEventListener('click', function (evt) {
+          
+        var randomIndex = Math.floor(Math.random() * COLORS.length);
+        this.setAttribute('material', 'color', COLORS[randomIndex]);
+        console.log('I was clicked at: ', evt.detail.intersection.point);
+      });
+    }
+  });
+  AFRAME.registerComponent('change-color-on-hover', {
+    init: function () {
+      var el = this.el;  // <a-box>
+
+      el.addEventListener('mouseenter', function () {
+        el.setAttribute('color', 'purple');
+      });
+
+      el.addEventListener('mouseleave', function () {
+        el.setAttribute('color', 'brown');
+      });
+    }
+  });
