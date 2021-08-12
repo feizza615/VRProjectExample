73a02584fda10501db85d869b8c96e59f11fbc12
Feizza Fazilatun <feizza01@gmail.com>
2021-08-11 20:45:33 -0400

    done

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+AFRAME.registerComponent('change-tree-color',{
+  init: function() {
+    var sceneEl = document.querySelector('a-scene'); 
+    sceneEl.querySelectorAll('a-cylinder')[0].setAttribute('material', {color: 'purple'});
+    sceneEl.querySelectorAll('a-cylinder')[1].setAttribute('material', {color: 'purple'});
+
+  }
+});
+
 AFRAME.registerComponent('cursor-listener', {
     init: function () {
       var COLORS = ['red', 'green', 'blue', 'yellow', 'purple'];
       this.el.addEventListener('click', function (evt) {
-          
         var randomIndex = Math.floor(Math.random() * COLORS.length);
         this.setAttribute('material', 'color', COLORS[randomIndex]);
         console.log('I was clicked at: ', evt.detail.intersection.point);
